39331bf677b1b31eb11b24537815e189b9432c7e
Mark Remmey <mremmey7@gmail.com>
2024-08-20 09:49:05 -0400

    Updates

--- constants.py
+++ constants.py
@@ -8,4 +8,4 @@ DB_USER = "markremmey"
 DB_PORT = "1433"
 DB_NAME = "sample-sql"
 
-ODBC_CONNECTION_STRING="Driver={ODBC Driver 18 for SQL Server};Server=tcp:sample-sql-remm.database.windows.net,1433;Database=sample-sql;Uid=markremmey;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+ODBC_CONNECTION_STRING="Driver={ODBC Driver 18 for SQL Server};Server=tcp:sample-sql-remm.database.windows.net,1433;Database=sample-sql;Uid=markremmey;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
